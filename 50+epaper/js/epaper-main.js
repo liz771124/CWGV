df1d64a1c6b7ccdca4fe3103eb874ce5b8ba95a0
ITPRESCO\ping.lin <ping.lin@presco.ws>
2023-12-20 13:48:20 +0800

    fix: Android 手機版分享修正為 line 分享

--- 50+epaper/js/epaper-main.js
+++ 50+epaper/js/epaper-main.js
@@ -7,12 +7,21 @@ let currentSection = 1,
   sidebar = document.querySelector(".sidebar"),
   gotop = document.querySelector(".gotop"),
   shareBtn = document.querySelector("#share__btn"),
+  lineBtn = document.querySelector("#line__btn"),
   subscribe = document.querySelector(".subscribe"),
   subscribeClose = document.querySelector(".subscribe-close"),
   headerHeight = header.offsetHeight,
   navbarHeight = navbar.offsetHeight,
-  chapterHeight = chapter.offsetHeight;
+  chapterHeight = chapter.offsetHeight,
+  isAndroid = navigator.userAgent.indexOf('Android') > -1 ;
 
+sidebar.style.top = `${navbar.offsetHeight + chapter.offsetHeight + 20}px`;
+
+if(isAndroid) {
+  lineBtn.style.display = "flex";
+  shareBtn.style.display = "none";
+}
+  
 const swiper = new Swiper(".chapter__slider", {
   loop: true,
   autoplay: {
@@ -43,24 +52,23 @@ window.addEventListener("scroll", () => {
   let scrollTop = document.documentElement.scrollTop;
   if (scrollTop > header.offsetHeight) {
     gotop.classList.add("gotopFixed");
-    subscribe.style.bottom = "0";
+    subscribe.style.bottom = "-2px";
     chapter.classList.add("show");
   } else {
     gotop.classList.remove("gotopFixed");
     chapter.classList.remove("show");
     subscribe.style.bottom = "-100%";
   }
-  sections.forEach((section, index) => {
-    const rect = section.getBoundingClientRect();
-    if (
-      rect.top <= window.innerHeight / 2 &&
-      rect.bottom >= window.innerHeight / 2
-    ) {
-      currentSection = section;
-      currentSectionIndex = index;
-    }
-  });
-  // swiper.slideToLoop(currentSectionIndex, 300, true);
+  // sections.forEach((section, index) => {
+  //   const rect = section.getBoundingClientRect();
+  //   if (
+  //     rect.top <= window.innerHeight / 2 &&
+  //     rect.bottom >= window.innerHeight / 2
+  //   ) {
+  //     currentSection = section;
+  //     currentSectionIndex = index;
+  //   }
+  // });
 });
 
 const anchors = document.querySelectorAll("a[href^='#section']");
@@ -113,5 +121,3 @@ subscribeClose.addEventListener("click", () => {
   subscribe.style.opacity = 0;
   subscribe.style.removeProperty("bottom");
 });
-
-sidebar.style.top = `${navbar.offsetHeight + chapter.offsetHeight + 20}px`;

2a907d00ed1979a57b0da26cd96433b3fba47056
liz771124 <liz771124@yahoo.com.tw>
2024-06-21 02:00:30 +0800

    fix: 專案 scss 設置

--- futuredata/src/js/main.js
+++ futuredata/src/js/main.js
@@ -1,2 +1,10 @@
-import { Collapse, initTWE } from "tw-elements";
-initTWE({ Collapse });
+// import { Collapse, initTWE } from "tw-elements";
+// initTWE({ Collapse });
+
+// document.getElementById("test-id").className +=
+//   "bg-indigo-700 text-white rounded";
+
+document.getElementById("test-id").addEventListener("click", () => {
+  console.log("123");
+  document.getElementById("test").click();
+});

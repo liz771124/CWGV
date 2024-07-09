b4440a62c86e8c9ec52fb49969fcfdbb569b8dfd
ITPRESCO\ping.lin <ping.lin@presco.ws>
2024-07-09 16:23:10 +0800

    fix: 專案資料夾結構全頁更換

--- futuredata/src/assets/js/magazine-detail.js
+++ futuredata/src/assets/js/magazine-detail.js
@@ -4,21 +4,29 @@ const offcanvasNote = document.getElementById("offcanvasNote");
 
 heartIcon.addEventListener("click", function (e) {
   if (e.target.src.includes("heart-red.svg")) {
-    e.target.src = "../../assets/images/icons/heart-gray.svg";
+    e.target.src = "../../assets/img/icons/heart-gray.svg";
   } else {
-    e.target.src = "../../assets/images/icons/heart-red.svg";
+    e.target.src = "../../assets/img/icons/heart-red.svg";
   }
 });
 
 noteIcon.addEventListener("click", function (e) {
   if (e.target.src.includes("note-blue.svg")) {
-    e.target.src = "../../assets/images/icons/note-gray.svg";
+    e.target.src = "../../assets/img/icons/note-gray.svg";
   } else {
-    e.target.src = "../../assets/images/icons/note-blue.svg";
+    e.target.src = "../../assets/img/icons/note-blue.svg";
   }
 });
 
 offcanvasNote.addEventListener("shown.twe.offcanvas", (e) => {
   const backdrop = document.querySelector("div[data-twe-backdrop-show]");
+  const noteBackdrop = document.querySelector("#note-backdrop");
+  console.log(noteBackdrop);
   backdrop.classList.add("pointer-events-none");
+  noteBackdrop.classList.remove("hidden");
+});
+
+offcanvasNote.addEventListener("hidden.twe.offcanvas", (e) => {
+  const noteBackdrop = document.querySelector("#note-backdrop");
+  noteBackdrop.classList.add("hidden");
 });

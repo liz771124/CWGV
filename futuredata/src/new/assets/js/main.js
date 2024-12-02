8bd3380d12356825d97e37ffef12ce14c329907f
liz771124 <liz771124@yahoo.com.tw>
2024-12-03 01:49:37 +0800

    fix: 新版全站頁面製作

--- futuredata/src/new/assets/js/main.js
+++ futuredata/src/new/assets/js/main.js
@@ -3,28 +3,41 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeMenu = document.getElementById("closeMenu");
   const menuContainer = document.getElementById("menuContainer");
   const menuItems = document.querySelectorAll(".menu-item");
+  const body = document.body;
 
   menuIcon.addEventListener("click", () => {
+    body.classList.add("menu-open");
+    gsap.set(menuItems, { x: -50, opacity: 0 });
     gsap.to(menuContainer, {
       x: 0,
       opacity: 1,
       duration: 0.5,
       ease: "power3.out",
+      display: "block",
     });
-
-    gsap.fromTo(
-      menuItems,
-      { x: -50, opacity: 0 },
-      { x: 0, opacity: 1, duration: 0.5, ease: "power3.out", stagger: 0.1 },
-    );
+    gsap.to(menuItems, {
+      x: 0,
+      opacity: 1,
+      duration: 0.5,
+      ease: "power3.out",
+      stagger: 0.25, // 滑入間隔
+    });
   });
 
+  // 關閉選單
   closeMenu.addEventListener("click", () => {
+    // 恢復滾動
+    body.classList.remove("menu-open");
+
+    // 選單收合
     gsap.to(menuContainer, {
-      x: "100%",
+      x: "-100%",
       opacity: 0,
       duration: 0.5,
       ease: "power3.in",
+      onComplete: () => {
+        menuContainer.style.display = "none"; // 隱藏選單
+      },
     });
   });
 });

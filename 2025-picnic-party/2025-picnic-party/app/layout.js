0f0e7dd77f04866d7084c8966ab8cd20effd61a2
liz771124 <liz771124@yahoo.com.tw>
2024-12-29 17:56:11 +0800

    fix: 親子官網全樣式更新 next 新專案測試

--- 2025-picnic-party/2025-picnic-party/app/layout.js
+++ 2025-picnic-party/2025-picnic-party/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className={`relative antialiased`}>
         <ThemeProvider>
           <UserProvider>{children}</UserProvider>
         </ThemeProvider>
--- 2025-picnic-party/2025-picnic-party/app/page.js
+++ 2025-picnic-party/2025-picnic-party/app/page.js
@@ -5,6 +5,7 @@ import gsap from 'gsap'
 import { useRef, useState } from 'react'
 import { useGSAP } from '@gsap/react'
 import Swiper from '@/components/Swiper'
+import { Accordion } from 'flowbite-react'
 
 import UserProfile from '@/components/UserProfile'
 
@@ -45,7 +46,7 @@ export default function Home() {
   const container = useRef()
   const circle = useRef()
 
-  const { contextSafe } = useGSAP({ scope: container }) // we can pass in a config object as the 1st parameter to make scoping simple
+  const { contextSafe } = useGSAP({ scope: container }) // we can pass in Link config object as the 1st parameter to make scoping simple
 
   // ✅ wrapped in contextSafe() - animation will be cleaned up correctly
   // selector text is scoped properly to the container.
@@ -75,65 +76,140 @@ export default function Home() {
 
   return (
     <>
-      <header id="header" className="fixed-top">
+      <nav className="sticky top-0 z-50">
         <div className="bg-primary-500 px-3 py-2">
           <div className="container flex items-center justify-between px-3">
             <h1>
-              <Link href="index.html">
+              <Link href="/">
                 <Image width={180} height={90} src="/img/logo.svg" alt="logo"></Image>
               </Link>
             </h1>
 
-            <nav>
-              <ul className="flex items-center gap-5">
-                <li>
-                  <Link className="text-xl font-bold text-white hover:text-gray-50" href="/">
-                    首頁
-                  </Link>
-                </li>
-                <li>
-                  <Link className="text-xl font-bold text-white hover:text-gray-50" href="/gift">
-                    報名拿好禮
-                  </Link>
-                </li>
-                <li>
-                  <Link className="text-xl font-bold text-white hover:text-gray-50" href="/game">
-                    主題體驗區
-                  </Link>
-                </li>
-                <li>
-                  <Link className="text-xl font-bold text-white hover:text-gray-50" href="/game">
-                    超人氣舞台秀
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+            <ul className="flex items-center gap-5">
+              <li>
+                <Link className="text-xl font-bold text-white hover:text-gray-50" href="/">
+                  首頁
+                </Link>
+              </li>
+              <li>
+                <Link className="text-xl font-bold text-white hover:text-gray-50" href="/gift">
+                  報名拿好禮
+                </Link>
+              </li>
+              <li>
+                <Link className="text-xl font-bold text-white hover:text-gray-50" href="/game">
+                  主題體驗區
+                </Link>
+              </li>
+              <li>
+                <Link className="text-xl font-bold text-white hover:text-gray-50" href="/game">
+                  超人氣舞台秀
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
+      </nav>
+      <header className="flex h-screen items-center justify-center bg-[url('/img/bg_all.gif')] bg-cover bg-no-repeat p-5">
+        <div className="flex flex-col gap-5">
+          <Link href="/">
+            <Image width={500} height={400} src="/img/date.png" alt="logo"></Image>
+          </Link>
+        </div>
       </header>
-      <div className="flex flex-col gap-5">
-        <div>未來親子野餐日陪伴孩子10週年啦！</div>
-        <div>不只是我們共創的里程碑，也象徵著「幸福永續」</div>
-        <div>期待能陪伴孩子，朝著更美好的未來邁進！</div>
-        <div>更多歡樂、甜蜜的親子時光，</div>
-        <div>就從今年的10歲生日派對開始吧！</div>
+      <div>
+        <div className="flex flex-col gap-5">
+          <div>未來親子野餐日陪伴孩子10週年啦！</div>
+          <div>不只是我們共創的里程碑，也象徵著「幸福永續」</div>
+          <div>期待能陪伴孩子，朝著更美好的未來邁進！</div>
+          <div>更多歡樂、甜蜜的親子時光，</div>
+          <div>就從今年的10歲生日派對開始吧！</div>
+        </div>
       </div>
+
+      <Accordion collapseAll>
+        <Accordion.Panel>
+          <Accordion.Title>What is Flowbite?</Accordion.Title>
+          <Accordion.Content>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Flowbite is an open-source library of interactive components built on top of Tailwind
+              CSS including buttons, dropdowns, modals, navbars, and more.
+            </p>
+            <p className="text-gray-500 dark:text-gray-400">
+              Check out this guide to learn how to&nbsp;
+              <a
+                href="https://flowbite.com/docs/getting-started/introduction/"
+                className="text-cyan-600 hover:underline dark:text-cyan-500"
+              >
+                get started&nbsp;
+              </a>
+              and start developing websites even faster with components on top of Tailwind CSS.
+            </p>
+          </Accordion.Content>
+        </Accordion.Panel>
+        <Accordion.Panel>
+          <Accordion.Title>Is there a Figma file available?</Accordion.Title>
+          <Accordion.Content>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Flowbite is first conceptualized and designed using the Figma software so everything
+              you see in the library has a design equivalent in our Figma file.
+            </p>
+            <p className="text-gray-500 dark:text-gray-400">
+              Check out the
+              <a
+                href="https://flowbite.com/figma/"
+                className="text-cyan-600 hover:underline dark:text-cyan-500"
+              >
+                Figma design system
+              </a>
+              based on the utility classes from Tailwind CSS and components from Flowbite.
+            </p>
+          </Accordion.Content>
+        </Accordion.Panel>
+        <Accordion.Panel>
+          <Accordion.Title>
+            What are the differences between Flowbite and Tailwind UI?
+          </Accordion.Title>
+          <Accordion.Content>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              The main difference is that the core components from Flowbite are open source under
+              the MIT license, whereas Tailwind UI is a paid product. Another difference is that
+              Flowbite relies on smaller and standalone components, whereas Tailwind UI offers
+              sections of pages.
+            </p>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI
+              as there is no technical reason stopping you from using the best of two worlds.
+            </p>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Learn more about these technologies:
+            </p>
+            <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
+              <li>
+                <a
+                  href="https://flowbite.com/pro/"
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Flowbite Pro
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://tailwindui.com/"
+                  rel="nofollow"
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Tailwind UI
+                </a>
+              </li>
+            </ul>
+          </Accordion.Content>
+        </Accordion.Panel>
+      </Accordion>
 
       <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
         <Swiper images={swiperImages} />
       </div>
-
-      <Link href="/about">ABOUT</Link>
-
-      <form action={formAction}>
-        <input type="text" name="item" placeholder="新增待辦..." />
-        <button type="submit">新增</button>
-      </form>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item.text}</li>
-        ))}
-      </ul>
 
       <div ref={container}>
         <button onClick={onClickGood} className="good h-20 w-20 bg-red-500">
@@ -163,7 +239,18 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="container mx-auto rounded-lg bg-white pb-8 pt-5 shadow-[0px_3px_8px_0px_rgba(0,0,0,0.3)] md:px-8">
+        <div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.3623542421947!2d121.47821782537727!3d25.05570482780247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a8fac6152a05%3A0x3821847e329dcd64!2z5paw5YyX5aSn6YO95pyD5YWs5ZyS!5e0!3m2!1szh-TW!2stw!4v1710127332403!5m2!1szh-TW!2stw"
+            width="600"
+            height="450"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+
+        <div className="container mx-auto md:px-8">
           <div className="mb-3 flex flex-wrap gap-8 py-5 md:flex-nowrap">
             <div className="mx-auto max-w-[350px]">
               <div className="ms-auto">
@@ -290,7 +377,7 @@ export default function Home() {
           </div>
           <div className="article-content py-3">
             <p>
-              <a href="#">我是連結區域</a>
+              <Link href="#">我是連結區域</Link>
             </p>
             <p>
               本教案由林心茹老師（基隆市仁愛國民小學）、王立衍老師（基隆市仁愛國民小學）共同編寫。
@@ -379,14 +466,14 @@ export default function Home() {
 
           <div className="mt-8 grid grid-cols-2 gap-8 rounded md:grid-cols-4">
             <div>
-              <a className="mb-2 block" rel="noopener noreferrer" href="#">
+              <Link className="mb-2 block" rel="noopener noreferrer" href="#">
                 <div className="flex max-w-full shrink-0 overflow-hidden rounded bg-gray-50">
                   <img
                     className="aspect-[1/1] h-full w-full object-contain"
                     src="https://imgs.cwgv.com.tw/top_project/44/244/preview/244.png"
                   />
                 </div>
-              </a>
+              </Link>
               <div className="text-center">
                 <h3 className="text-lg mb-2 line-clamp-2 font-semibold">
                   太陽的親戚太陽的親戚太陽的親戚太陽的親戚太陽的親戚
@@ -397,6 +484,16 @@ export default function Home() {
           </div>
         </div>
       </main>
+
+      <div className="py-5">
+        <div className="container">
+          <div className="grid grid-cols-2 md:grid-cols-4">
+            <Link href="#" target="_blank">
+              <Image src="/img/logo01.jpg" width="200" height="50" alt="" />
+            </Link>
+          </div>
+        </div>
+      </div>
 
       <footer className="tracking-wide">
         <div className="container mx-auto flex flex-wrap items-center justify-between gap-5 px-3 py-5 text-sm">

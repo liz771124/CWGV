9248ee34734add0eecf14bc47a2694bf8640d2b5
liz771124 <liz771124@yahoo.com.tw>
2024-12-30 03:03:35 +0800

    fix: next config 更新

--- 2025-picnic-party/2025-picnic-party/app/page.js
+++ 2025-picnic-party/2025-picnic-party/app/page.js
@@ -5,9 +5,8 @@ import gsap from 'gsap'
 import { useRef, useState } from 'react'
 import { useGSAP } from '@gsap/react'
 import Swiper from '@/components/Swiper'
-import { Accordion } from 'flowbite-react'
 
-import UserProfile from '@/components/UserProfile'
+
 
 const swiperImages = [
   {
@@ -127,85 +126,7 @@ export default function Home() {
         </div>
       </div>
 
-      <Accordion collapseAll>
-        <Accordion.Panel>
-          <Accordion.Title>What is Flowbite?</Accordion.Title>
-          <Accordion.Content>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is an open-source library of interactive components built on top of Tailwind
-              CSS including buttons, dropdowns, modals, navbars, and more.
-            </p>
-            <p className="text-gray-500 dark:text-gray-400">
-              Check out this guide to learn how to&nbsp;
-              <a
-                href="https://flowbite.com/docs/getting-started/introduction/"
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                get started&nbsp;
-              </a>
-              and start developing websites even faster with components on top of Tailwind CSS.
-            </p>
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title>Is there a Figma file available?</Accordion.Title>
-          <Accordion.Content>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Flowbite is first conceptualized and designed using the Figma software so everything
-              you see in the library has a design equivalent in our Figma file.
-            </p>
-            <p className="text-gray-500 dark:text-gray-400">
-              Check out the
-              <a
-                href="https://flowbite.com/figma/"
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Figma design system
-              </a>
-              based on the utility classes from Tailwind CSS and components from Flowbite.
-            </p>
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title>
-            What are the differences between Flowbite and Tailwind UI?
-          </Accordion.Title>
-          <Accordion.Content>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              The main difference is that the core components from Flowbite are open source under
-              the MIT license, whereas Tailwind UI is a paid product. Another difference is that
-              Flowbite relies on smaller and standalone components, whereas Tailwind UI offers
-              sections of pages.
-            </p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI
-              as there is no technical reason stopping you from using the best of two worlds.
-            </p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">
-              Learn more about these technologies:
-            </p>
-            <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-              <li>
-                <a
-                  href="https://flowbite.com/pro/"
-                  className="text-cyan-600 hover:underline dark:text-cyan-500"
-                >
-                  Flowbite Pro
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://tailwindui.com/"
-                  rel="nofollow"
-                  className="text-cyan-600 hover:underline dark:text-cyan-500"
-                >
-                  Tailwind UI
-                </a>
-              </li>
-            </ul>
-          </Accordion.Content>
-        </Accordion.Panel>
-      </Accordion>
+
 
       <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
         <Swiper images={swiperImages} />
@@ -225,7 +146,7 @@ export default function Home() {
       <div className="box h-10 w-10 rounded-full bg-blue-400">selector</div>
 
       <main className="py-6">
-        <UserProfile />
+
 
         <div>
           <div className="py-3 text-center text-xl font-bold">

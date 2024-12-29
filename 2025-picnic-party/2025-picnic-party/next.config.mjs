930073bfd3768ed66a9cac76f087aa7731b12eac
liz771124 <liz771124@yahoo.com.tw>
2024-12-29 18:26:40 +0800

    fix: next 新專案測試

--- 2025-picnic-party/2025-picnic-party/next.config.mjs
+++ 2025-picnic-party/2025-picnic-party/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: '.', // 使用相對路徑來處理資源
+  assetPrefix: '/CWGV/2025-picnic-party/', // 根據你的 GitHub Pages 路徑設定
+  basePath: '/CWGV/2025-picnic-party', // 同樣設定為專案路徑
   images: {
-    // 本地圖片與第三方圖片來源混用
-    unoptimized: true, // 禁用 Next.js 內建圖片優化（適用於靜態匯出）
-    domains: ['imgs.cwgv.com.tw'] // 允許外部圖片來源
+    unoptimized: true, // 禁用 Next.js 內建圖片優化
+    domains: ['imgs.cwgv.com.tw'] // 外部圖片來源
   },
   trailingSlash: true // 確保靜態路徑末尾有 `/`
 }
